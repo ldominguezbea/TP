44fb0aafa1c3beaa2ddb3f23753d567fb89c295d
IgnPine <ipineda@alumno.huergo.edu.ar>
2026-09-14 08:22:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ WIDTH = 1350
 HEIGHT = 700
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Beat Em Up")
+pygame.display.set_caption("REQUIEM")
 
 clock = pygame.time.Clock()
-
 
 class Player:
     def __init__(self, x, y):
@@ -18,23 +17,18 @@ class Player:
         self.speed = 5
         self.health = 150
         self.max_health = 150
-
         self.direction = "right"
-
         self.attacking = False
         self.attack_timer = 0
         self.attack_cooldown = 0
         self.attack_cooldown_time = 0.3
         self.has_hit = False
-
         self.hurtbox = pygame.Rect(
             x, y, 60, 90
         )
-
         self.hitbox = pygame.Rect(
             0, 0, 0, 0
         )
-
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -51,262 +45,191 @@ class Player:
 
         if keys[pygame.K_s]:
             self.rect.y += self.speed
-
         self.rect.x = max(
             0,
             min(WIDTH - self.rect.width, self.rect.x)
         )
-
         self.rect.y = max(
             10,
             min(590 - self.rect.height, self.rect.y)
         )
-
         self.hurtbox.topleft = self.rect.topleft
-
         if self.attack_cooldown > 0:
             self.attack_cooldown -= dt
-
         if self.attacking:
-
             if self.direction == "right":
                 self.hitbox.topleft = (
                     self.rect.right,
                     self.rect.y + 20
                 )
-
             else:
                 self.hitbox.topleft = (
                     self.rect.left - 50,
                     self.rect.y + 20
                 )
-
             self.attack_timer -= dt
-
             if self.attack_timer <= 0:
                 self.attacking = False
                 self.has_hit = False
                 self.hitbox = pygame.Rect(
                     0, 0, 0, 0
                 )
-
     def attack(self):
-
         if self.attack_cooldown > 0:
             return
-
         if self.attacking:
             return
-
         self.attacking = True
         self.attack_timer = 0.15
         self.attack_cooldown = self.attack_cooldown_time
         self.has_hit = False
-
         if self.direction == "right":
-
             self.hitbox = pygame.Rect(
                 self.rect.right,
                 self.rect.y + 20,
                 50,
                 50
             )
-
         else:
-
             self.hitbox = pygame.Rect(
                 self.rect.left - 50,
                 self.rect.y + 20,
                 50,
                 50
             )
-
     def take_damage(self, damage):
-
         self.health -= damage
-
         print("Jugador recibió", damage, "de daño")
         print("Vida del jugador:", self.health)
-
     def draw(self):
-
         pygame.draw.rect(
             screen,
             (0, 100, 255),
             self.rect
         )
-
         pygame.draw.rect(
             screen,
             (0, 255, 0),
             self.hurtbox,
             2
         )
-
         if self.attacking:
-
             pygame.draw.rect(
                 screen,
                 (255, 255, 0),
                 self.hitbox,
                 2
             )
-
-
 class Enemy:
     def __init__(self, x, y):
-
         self.rect = pygame.Rect(
             x, y, 60, 90
         )
-
         self.speed = 2
         self.health = 120
         self.max_health = 120
-
         self.direction = "left"
-
         self.hurtbox = pygame.Rect(
             x, y, 60, 90
         )
-
         self.hitbox = pygame.Rect(
             0, 0, 0, 0
         )
-
         self.attacking = False
         self.attack_timer = 0
-
         self.attack_cooldown = 0
         self.attack_cooldown_time = 0.8
-
         self.has_hit = False
-
     def update(self, player, dt):
-
         if self.rect.x < player.rect.x:
             self.rect.x += self.speed
             self.direction = "right"
-
         elif self.rect.x > player.rect.x:
             self.rect.x -= self.speed
             self.direction = "left"
-
         if self.rect.y < player.rect.y:
             self.rect.y += self.speed
-
         elif self.rect.y > player.rect.y:
             self.rect.y -= self.speed
-
         self.rect.x = max(
             0,
             min(WIDTH - self.rect.width, self.rect.x)
         )
-
         self.rect.y = max(
             10,
             min(590 - self.rect.height, self.rect.y)
         )
-
         self.hurtbox.topleft = self.rect.topleft
-
         if self.attack_cooldown > 0:
             self.attack_cooldown -= dt
-
         distance_x = abs(
             self.rect.centerx -
             player.rect.centerx
         )
-
         distance_y = abs(
             self.rect.centery -
             player.rect.centery
         )
-
         if distance_x < 100 and distance_y < 80:
-
             if not self.attacking:
                 self.attack()
-
         if self.attacking:
-
             if self.direction == "right":
-
                 self.hitbox.topleft = (
                     self.rect.right,
                     self.rect.y + 20
                 )
-
             else:
-
                 self.hitbox.topleft = (
                     self.rect.left - 50,
                     self.rect.y + 20
                 )
-
             self.attack_timer -= dt
-
             if self.attack_timer <= 0:
-
                 self.attacking = False
                 self.has_hit = False
-
                 self.hitbox = pygame.Rect(
                     0, 0, 0, 0
                 )
-
     def attack(self):
-
         if self.attack_cooldown > 0:
             return
-
         self.attacking = True
         self.attack_timer = 0.15
         self.attack_cooldown = self.attack_cooldown_time
         self.has_hit = False
-
         if self.direction == "right":
-
             self.hitbox = pygame.Rect(
                 self.rect.right,
                 self.rect.y + 20,
                 50,
                 50
             )
-
         else:
-
             self.hitbox = pygame.Rect(
                 self.rect.left - 50,
                 self.rect.y + 20,
                 50,
                 50
             )
-
     def take_damage(self, damage):
-
         self.health -= damage
-
         print("Enemigo recibió", damage, "de daño")
         print("Vida del enemigo:", self.health)
 
     def draw(self):
-
         pygame.draw.rect(
             screen,
             (255, 50, 50),
             self.rect
         )
-
         pygame.draw.rect(
             screen,
             (0, 255, 0),
             self.hurtbox,
             2
         )
-
         if self.attacking:
-
             pygame.draw.rect(
                 screen,
                 (255, 150, 0),
