5415a49a2b83a71135bb113b9ea9bfc7c3bf0cc4
nickk <ifalcone@alumno.huergo.edu.ar>
2026-09-16 09:14:09 -0300

    Update enemigos.py

--- enemigos.py
+++ enemigos.py
@@ -22,15 +22,3 @@ esqueleto_negro= esqueletoNegro()
 
 enemigos = [OrcoRojo, esqueletoNegro]
 
-def crear_enemigos():
-    enemigo = random.choice(enemigos)
-    v_ataque = 5
-    spawn = USEREVENT.
-    while enemigo.vida != 0:
-        pygame.time.set_timer()
-        pass
-crear_enemigos()
-
-def hola():
-    print("Hola")
-    
